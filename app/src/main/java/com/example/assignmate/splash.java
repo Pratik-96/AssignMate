ebdf4d9e626d722bfa7c118d99974c56e98518df
AnonymousCoder1 <pratiknandvikar1404@gmail.com>
2023-06-16 23:15:51 +0530

    Added a Tag line to the splash activity while loading the App.

--- app/src/main/java/com/example/assignmate/splash.java
+++ app/src/main/java/com/example/assignmate/splash.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+
+import com.example.assignmate.databinding.ActivitySplashBinding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -13,11 +15,13 @@ public class splash extends AppCompatActivity {
 
     FirebaseAuth mAuth;
     FirebaseUser user;
+    private ActivitySplashBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        binding=ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         user = FirebaseAuth.getInstance().getCurrentUser();
 
@@ -36,7 +40,93 @@ public class splash extends AppCompatActivity {
                 }
 
             }
-        },1500);
+        },2000);
 
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                binding.appname.setText("A");
+            }
+        },300);
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                binding.appname.append("S");
+            }
+        },400);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                binding.appname.append("S");
+            }
+        },500);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                binding.appname.append("I");
+            }
+        },600);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                binding.appname.append("G");
+            }
+        },700);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                binding.appname.append("N");
+            }
+        },800);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                binding.appname.append("M");
+            }
+        },900);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                binding.appname.append("A");
+            }
+        },1000);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                binding.appname.append("T");
+            }
+        },1100);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                binding.appname.append("E");
+            }
+        },1200);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                binding.moto.setText("Share, Collaborate, Excel!");
+            }
+        },1300);
     }
 }

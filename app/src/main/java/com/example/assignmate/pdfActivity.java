8ae65b000c98b08de6a0b415fc4c4a400bbefaa7
AnonymousCoder1 <pratiknandvikar1404@gmail.com>
2023-07-16 22:35:42 +0530

    Added feedback option in Profile

---
+++ app/src/main/java/com/example/assignmate/pdfActivity.java
@@ -0,0 +1,109 @@
+package com.example.assignmate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.listener.OnErrorListener;
+import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+
+public class pdfActivity extends AppCompatActivity {
+
+    PDFView pdfView;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdf);
+
+         pdfView = findViewById(R.id.pdfView);
+        TextView name = findViewById(R.id.file_name);
+        ImageView download = findViewById(R.id.download);
+        ImageView back = findViewById(R.id.backtofetch);
+
+         progressBar = findViewById(R.id.progressBar);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
+
+        String fname = getIntent().getStringExtra("NAME");
+        name.setText(fname);
+
+
+        String link = getIntent().getStringExtra("URL");
+
+
+
+        download.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(link)));
+            }
+        });
+
+        new pdfDownload().execute(link);
+
+
+    }
+
+    public class pdfDownload extends AsyncTask<String,Void, InputStream>
+    {
+
+        @Override
+        protected InputStream doInBackground(String... strings) {
+            InputStream inputStream = null;
+            try {
+                java.net.URL url = new URL(strings[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                if (connection.getResponseCode()==200)
+                {
+                    inputStream= new BufferedInputStream(connection.getInputStream());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return inputStream;
+        }
+
+        @Override
+        protected void onPostExecute(InputStream inputStream) {
+            pdfView.fromStream(inputStream)
+                    .onLoad(new OnLoadCompleteListener() {
+                        @Override
+                        public void loadComplete(int nbPages) {
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    })
+                    .spacing(10)
+                    .load();
+        }
+
+
+    }
+}

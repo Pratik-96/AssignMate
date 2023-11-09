66a219bf7b0b223f593fb899e79522dda154fa11
AnonymousCoder1 <pratiknandvikar1404@gmail.com>
2023-11-09 22:52:02 +0530

    Added new choose semester feature to expand AssignMate..

---
+++ app/src/main/java/com/example/assignmate/User.java
@@ -0,0 +1,39 @@
+package com.example.assignmate;
+
+public class User {
+    private String uid;
+
+    private String name;
+
+    private String sem;
+
+    public User(String uid, String name, String sem) {
+        this.uid = uid;
+        this.name = name;
+        this.sem = sem;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSem() {
+        return sem;
+    }
+
+    public void setSem(String sem) {
+        this.sem = sem;
+    }
+}
---
+++ app/src/main/java/com/example/assignmate/choose_sem.java
@@ -0,0 +1,73 @@
+package com.example.assignmate;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.example.assignmate.databinding.ActivityChooseSemBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class choose_sem extends AppCompatActivity {
+
+    ActivityChooseSemBinding binding;
+
+    RadioButton radioButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityChooseSemBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        String name = getIntent().getStringExtra("uname");
+
+
+        final String[] selectedSem = {""};
+        binding.radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                int id = radioGroup.getCheckedRadioButtonId();
+                radioButton = findViewById(id);
+
+                Log.d("radio", "onCreate: "+radioButton.getText());
+                selectedSem[0] =radioButton.getText().toString();
+
+            }
+        });
+
+
+        binding.submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Users").push();
+                User u = new User(FirebaseAuth.getInstance().getUid(),name,selectedSem[0]);
+                reference.setValue(u).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful())
+                        {
+                            Toast.makeText(choose_sem.this, "Welcome "+name+" to AssignMate..!!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),Login.class));
+                            finish();
+                        }
+                    }
+                });
+            }
+        });
+
+
+
+
+    }
+}
